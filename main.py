ba3c191d3a490f871736b5ebcb154039458f67d2
Kelly Patel <kelly_patel@brown.edu>
2021-11-15 14:11:31 -0500

    Added main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+CSCI 1430 Computer Vision Final project
+By Cheng-You Lu, Ji Won Chung, Kelly Patel
+
+Usage:
+    
+"""
+import os
+import sys
+import argparse
+
+if __name__ == '__main__':
+    print("In main.py")
