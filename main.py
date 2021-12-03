dd0d22ee729dd1cf5a5ac493cb794717d541dc83
Kelly Patel <kelly_patel@brown.edu>
2021-12-02 21:10:52 -0500

    Fixed RGB

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def test(model, test_generator):
                     # print("Normalized!", norm_frame)
                     # im = Image.fromarray(norm_frame)      
                     print("Normalized!", norm_frame)
-                    norm = cv2.cvtColor(norm_frame, cv2.COLOR_BGR2RBG)
+                    norm = cv2.cvtColor(norm_frame, cv2.COLOR_BGR2RGB)
                     res = cv2.imwrite(path, norm)
                     # im.save(path)
             elif type == "FAKE" and fake_count < 3:
@@ -111,7 +111,7 @@ def test(model, test_generator):
                     # print("Normalized!", norm_frame)
                     # im = Image.fromarray(norm_frame)      
                     print("Normalized!", norm_frame)
-                    norm = cv2.cvtColor(norm_frame, cv2.COLOR_BGR2RBG)
+                    norm = cv2.cvtColor(norm_frame, cv2.COLOR_BGR2RGB)
                     res = cv2.imwrite(path, norm)
                     # im.save(path)
             print("Written image!", res)
@@ -128,7 +128,7 @@ def test(model, test_generator):
                     # print("Normalized!", norm_frame)
                     # im = Image.fromarray(norm_frame)      
                     print("Normalized!", norm_frame)
-                    norm = cv2.cvtColor(norm_frame, cv2.COLOR_BGR2RBG)
+                    norm = cv2.cvtColor(norm_frame, cv2.COLOR_BGR2RGB)
                     res = cv2.imwrite(path, norm)
                     # im.save(path)
 
